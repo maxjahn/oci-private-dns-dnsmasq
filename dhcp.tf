c827e1bf5853267984d6b4643b24a9e754abae35
maxjahn <max.jahn@gmail.com>
2020-04-27 14:50:26 +0200

    updated to terraform 0.12

--- dhcp.tf
+++ dhcp.tf
@@ -1,18 +1,16 @@
-
 resource "oci_core_dhcp_options" "pdns_dhcp_options" {
-    compartment_id = "${var.oci_compartment_ocid}"
-    options {
-        type = "DomainNameServer"
-        server_type = "CustomDnsServer"
-        custom_dns_servers = [ "${oci_core_instance.dns_forward_instance_01.private_ip}" , "${oci_core_instance.dns_forward_instance_02.private_ip}",  "169.254.169.254" ]
-    }
+  compartment_id = var.oci_compartment_ocid
+  options {
+    type               = "DomainNameServer"
+    server_type        = "CustomDnsServer"
+    custom_dns_servers = [oci_core_instance.dns_forward_instance_01.private_ip, oci_core_instance.dns_forward_instance_02.private_ip, "169.254.169.254"]
+  }
 
-    options {
-        type = "SearchDomain"
-        search_domain_names = [ "${var.private_dns_domain}" ]
-    }
+  options {
+    type                = "SearchDomain"
+    search_domain_names = [var.private_dns_domain]
+  }
 
-    vcn_id = "${oci_core_virtual_network.pdns_vcn.id}"
+  vcn_id = oci_core_virtual_network.pdns_vcn.id
 }
-
 
--- network.tf
+++ network.tf
@@ -1,57 +1,56 @@
 resource "oci_core_virtual_network" "pdns_vcn" {
-  cidr_block     = "${var.oci_cidr_vcn}"
+  cidr_block     = var.oci_cidr_vcn
   dns_label      = "pdnsvcn"
-  compartment_id = "${var.oci_compartment_ocid}"
+  compartment_id = var.oci_compartment_ocid
   display_name   = "pdns-vcn"
 }
 
 resource "oci_core_subnet" "dns_forward_subnet" {
-  cidr_block        = "${var.oci_cidr_forward_subnet}"
-  compartment_id    = "${var.oci_compartment_ocid}"
-  vcn_id            = "${oci_core_virtual_network.pdns_vcn.id}"
+  cidr_block        = var.oci_cidr_forward_subnet
+  compartment_id    = var.oci_compartment_ocid
+  vcn_id            = oci_core_virtual_network.pdns_vcn.id
   display_name      = "dns-forward-subnet"
-  dns_label      = "dnsfwsubnet"
-  security_list_ids = ["${oci_core_security_list.pdns_sl.id}"]
+  dns_label         = "dnsfwsubnet"
+  security_list_ids = [oci_core_security_list.pdns_sl.id]
 }
 
 resource "oci_core_subnet" "private_subnet" {
-  cidr_block        = "${var.oci_cidr_private_subnet}"
-  compartment_id    = "${var.oci_compartment_ocid}"
-  vcn_id            = "${oci_core_virtual_network.pdns_vcn.id}"
+  cidr_block        = var.oci_cidr_private_subnet
+  compartment_id    = var.oci_compartment_ocid
+  vcn_id            = oci_core_virtual_network.pdns_vcn.id
   display_name      = "private-subnet"
-  dns_label      = "privsubnet"
-  dhcp_options_id = "${oci_core_dhcp_options.pdns_dhcp_options.id}" 
-  security_list_ids = ["${oci_core_security_list.private_sl.id}"]
+  dns_label         = "privsubnet"
+  dhcp_options_id   = oci_core_dhcp_options.pdns_dhcp_options.id
+  security_list_ids = [oci_core_security_list.private_sl.id]
 }
 
 resource "oci_core_subnet" "public_subnet" {
-  cidr_block        = "${var.oci_cidr_public_subnet}"
-  compartment_id    = "${var.oci_compartment_ocid}"
-  vcn_id            = "${oci_core_virtual_network.pdns_vcn.id}"
-  display_name      = "public-subnet"
-  dns_label      = "pubsubnet"
-  dhcp_options_id = "${oci_core_dhcp_options.pdns_dhcp_options.id}" 
+  cidr_block      = var.oci_cidr_public_subnet
+  compartment_id  = var.oci_compartment_ocid
+  vcn_id          = oci_core_virtual_network.pdns_vcn.id
+  display_name    = "public-subnet"
+  dns_label       = "pubsubnet"
+  dhcp_options_id = oci_core_dhcp_options.pdns_dhcp_options.id
 }
 
 resource "oci_core_internet_gateway" "pdns_igw" {
   display_name   = "pdns-internet-gateway"
-  compartment_id = "${var.oci_compartment_ocid}"
-  vcn_id         = "${oci_core_virtual_network.pdns_vcn.id}"
+  compartment_id = var.oci_compartment_ocid
+  vcn_id         = oci_core_virtual_network.pdns_vcn.id
 }
 
-
 resource "oci_core_default_route_table" "default_route_table" {
-  manage_default_resource_id = "${oci_core_virtual_network.pdns_vcn.default_route_table_id}"
+  manage_default_resource_id = oci_core_virtual_network.pdns_vcn.default_route_table_id
 
   route_rules {
-    network_entity_id = "${oci_core_internet_gateway.pdns_igw.id}"
+    network_entity_id = oci_core_internet_gateway.pdns_igw.id
     destination       = "0.0.0.0/0"
   }
 }
 
 resource "oci_core_security_list" "pdns_sl" {
-  compartment_id = "${var.oci_compartment_ocid}"
-  vcn_id            = "${oci_core_virtual_network.pdns_vcn.id}"
+  compartment_id = var.oci_compartment_ocid
+  vcn_id         = oci_core_virtual_network.pdns_vcn.id
   display_name   = "pdns-security-list"
 
   ingress_security_rules {
@@ -59,7 +58,7 @@ resource "oci_core_security_list" "pdns_sl" {
     protocol = "1"
   }
 
-ingress_security_rules {
+  ingress_security_rules {
     source   = "0.0.0.0/0"
     protocol = "6"
 
@@ -70,7 +69,7 @@ ingress_security_rules {
   }
 
   ingress_security_rules {
-    source   = "${var.oci_cidr_vcn}"
+    source   = var.oci_cidr_vcn
     protocol = "6"
 
     tcp_options {
@@ -80,7 +79,7 @@ ingress_security_rules {
   }
 
   ingress_security_rules {
-    source   = "${var.oci_cidr_vcn}"
+    source   = var.oci_cidr_vcn
     protocol = "17"
 
     udp_options {
@@ -90,16 +89,14 @@ ingress_security_rules {
   }
 
   egress_security_rules {
-    destination   = "0.0.0.0/0"
-    protocol = "all"
-    }
-
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
 }
 
-
 resource "oci_core_security_list" "private_sl" {
-  compartment_id = "${var.oci_compartment_ocid}"
-  vcn_id            = "${oci_core_virtual_network.pdns_vcn.id}"
+  compartment_id = var.oci_compartment_ocid
+  vcn_id         = oci_core_virtual_network.pdns_vcn.id
   display_name   = "private-security-list"
 
   ingress_security_rules {
@@ -107,7 +104,7 @@ resource "oci_core_security_list" "private_sl" {
     protocol = "1"
   }
 
-ingress_security_rules {
+  ingress_security_rules {
     source   = "10.0.0.0/16"
     protocol = "6"
 
@@ -118,20 +115,8 @@ ingress_security_rules {
   }
 
   egress_security_rules {
-    destination   = "0.0.0.0/0"
-    protocol = "all"
-    }
-
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
 
